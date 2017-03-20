e35f6139c9d8495a92a9a6549da8331f75855264
Pavel Ilchev <pavel_ilchev@abv.bg>
2017-03-20 20:06:25 +0200

    cozmetics

--- OnlineStore/Store.App/App_Start/BundleConfig.cs
+++ OnlineStore/Store.App/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace Store.App
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                    "~/Scripts/adminpanel.js"));
         }
     }
 }
---
+++ OnlineStore/Store.App/Scripts/adminpanel.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("[data-pdsa-action]").on("click", function (e) {
+        e.preventDefault();
+
+        $("#EventCommand").val(
+          $(this).data("pdsa-action"));
+
+        $("#EventArgument").val(
+          $(this).attr("data-pdsa-val"));
+
+        $("form").submit();
+    });
+});
