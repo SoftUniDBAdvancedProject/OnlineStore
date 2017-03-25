719b44461a02ab498a87a6b46a945af1c870a2c0
Bogdan Alov <alovbg21@abv.bg>
2017-03-25 22:17:30 +0200

    added SeedProducts (not converted)

--- OnlineStore/Store.Data/Migrations/Configuration.cs
+++ OnlineStore/Store.Data/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace Store.Data.Migrations
 {
+    using System;
     using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.IO;
@@ -24,6 +25,303 @@ namespace Store.Data.Migrations
             this.SeedAdmin(context);
             this.SeedCountries(context);
             this.SeedCategories(context);
+            this.SeedProducts(context);
+        }
+
+        private void SeedProducts(StoreContext context)
+        {
+            var products = new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Harry Potter and The Deathly Hallows",
+                    Country = context.Countries.Where(c=> c.Name == "Ireland").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "BooksConsumptives").FirstOrDefault(),
+                    Price = 20,
+                    Description = "Harry Potter and the Deathly Hallows is the final book of the LEGENDARY collection Harry Potter by JK Rowling",
+                    Quantity = 100,
+                    PicturePath = "HarryPotterAndTheDeathlyHallows.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "It",
+                    Country = context.Countries.Where(c=> c.Name == "Ireland").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "BooksConsumptives").FirstOrDefault(),
+                    Price = 19.99m,
+                    Description = "Stephen King's IT is a best-seller.",
+                    Quantity = 150,
+                    PicturePath = "StephenKingIt.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Hunger Games (All Books)",
+                    Country = context.Countries.Where(c=> c.Name == "Ireland").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "BooksConsumptives").FirstOrDefault(),
+                    Price = 20,
+                    Description = "Harry Potter and the Deathly Hallows is the final book of the LEGENDARY collection Harry Potter",
+                    Quantity = 100,
+                    PicturePath = "HarryPotterAndTheDeathlyHallows.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Alienware 17 inches",
+                    Country = context.Countries.Where(c=> c.Name == "Japan").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "ComputersPeriphery").FirstOrDefault(),
+                    Price = 4999,
+                    Description = "Alienware is one of the best brands for GAMING Laptops in the WORLD",
+                    Quantity = 20,
+                    Warranty = 36,
+                    PicturePath = "alienware.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Acer Predator G9 15,6 inches",
+                    Country = context.Countries.Where(c=> c.Name == "Belgium").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "ComputersPeriphery").FirstOrDefault(),
+                    Price = 2000,
+                    Description = "Acer Predator is a very good gaming laptop for any type of game! And it's very cheap for it's characteristics",
+                    Quantity = 25,
+                    PicturePath = "acer-predator-g9.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Lenovo IDEAPAD 300 15,6 INCHES",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "ComputersPeriphery").FirstOrDefault(),
+                    Price = 999.99m,
+                    Description = "A very good LAPTOP for everything. I7 processor. Gotta lovei t",
+                    Quantity = 10,
+                    PicturePath = "lenovo_ideapad_300.png"
+
+                },
+
+                new Product()
+                {
+                    Name = "Laundry",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "ElectricAppliances").FirstOrDefault(),
+                    Price =200,
+                    Description = "A very good laundry",
+                    Quantity = 100,
+                    PicturePath = "laundry.png"
+
+                },
+                new Product()
+                {
+                    Name = "Stove",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "ElectricAppliances").FirstOrDefault(),
+                    Price = 250,
+                    Description = "A very good stove",
+                    Quantity = 100,
+                    PicturePath = "stove.png"
+
+                },
+                new Product()
+                {
+                    Name = "TV",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "ElectricAppliances").FirstOrDefault(),
+                    Price =1000,
+                    Description = "A very good TV",
+                    Quantity = 10,
+                    PicturePath = "TV.png"
+
+                },
+                new Product()
+                {
+                    Name = "Jeans",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Fashion").FirstOrDefault(),
+                    Price =60,
+                    Description = "Very good jeans",
+                    Quantity = 100,
+                    PicturePath = "jeans.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "T-shirt",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Fashion").FirstOrDefault(),
+                    Price =20,
+                    Description = "Very good t-shirt",
+                    Quantity = 100,
+                    PicturePath = "t-shirt.png"
+
+                },
+                new Product()
+                {
+                    Name = "Stool",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "HomeFurniture").FirstOrDefault(),
+                    Price = 39.99m,
+                    Description = "Very good stool",
+                    Quantity = 100,
+                    PicturePath = "stool.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Table",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "HomeFurniture").FirstOrDefault(),
+                    Price = 140.99m,
+                    Description = "Very good table",
+                    Quantity = 100,
+                    PicturePath = "table.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Bedchamber for Couple",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "HomeFurniture").FirstOrDefault(),
+                    Price = 239.99m,
+                    Description = "Very good bed chamber for a lovely couple",
+                    Quantity = 100,
+                    PicturePath = "bedchamber.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Caterpillar",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "MobileDevices").FirstOrDefault(),
+                    Price = 209.99m,
+                    Description = "Very good phone for angry people!",
+                    Quantity = 100,
+                    PicturePath = "caterpillar.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Huawei P10",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "MobileDevices").FirstOrDefault(),
+                    Price = 609.99m,
+                    Description = "Very good phone for cool people!",
+                    Quantity = 100,
+                    PicturePath = "huaweiP10.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Iphone 7 PLUS Rose Gold",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "MobileDevices").FirstOrDefault(),
+                    Price = 1449.99m,
+                    Description = "Very good phone for rich people!",
+                    Quantity = 100,
+                    PicturePath = "iphone7_rose_gold_128gb.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Nikon",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "PhotoVideo").FirstOrDefault(),
+                    Price = 1449.99m,
+                    Description = "Very good camera.",
+                    Quantity = 100,
+                    PicturePath = "nikon.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Fujifilm",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "PhotoVideo").FirstOrDefault(),
+                    Price = 1649.99m,
+                    Description = "Very good camera.",
+                    Quantity = 100,
+                    PicturePath = "fujifilm.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Canon",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "PhotoVideo").FirstOrDefault(),
+                    Price = 1949.99m,
+                    Description = "Very good camera.",
+                    Quantity = 100,
+                    PicturePath = "canon-slr.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Basket ball",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Sports").FirstOrDefault(),
+                    Price = 59.99m,
+                    Description = "Very good ball.",
+                    Quantity = 100,
+                    PicturePath = "basket-ball.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Football ball",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Sports").FirstOrDefault(),
+                    Price = 59.99m,
+                    Description = "Very good ball.",
+                    Quantity = 100,
+                    PicturePath = "foot-ball.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Golf stick",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Sports").FirstOrDefault(),
+                    Price = 159.99m,
+                    Description = "Very good stick.",
+                    Quantity = 100,
+                    PicturePath = "golf-stick.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "ATV for Kids",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Toys").FirstOrDefault(),
+                    Price = 79.99m,
+                    Description = "Very good ATV for kids.",
+                    Quantity = 100,
+                    PicturePath = "kids-ride-atv.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Real Steal Robots",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Toys").FirstOrDefault(),
+                    Price = 79.99m,
+                    Description = "Toy Robots for kids.",
+                    Quantity = 100,
+                    PicturePath = "real-steal.jpg"
+
+                },
+                new Product()
+                {
+                    Name = "Trucks",
+                    Country = context.Countries.Where(c=> c.Name == "Hong Kong").FirstOrDefault(),
+                    Category = context.Categories.Where(c=> c.Name == "Toys").FirstOrDefault(),
+                    Price = 39.99m,
+                    Description = "Toy Robots for kids.",
+                    Quantity = 100,
+                    PicturePath = "trucks_allcolors.jpg"
+
+                },
+            };
+
         }
 
         private void SeedCategories(StoreContext context)
@@ -51,7 +349,7 @@ namespace Store.Data.Migrations
                 Name = "Fashion",
                 PicturePath = "fashion.png"
             };
-            
+
             var home = new Category()
             {
                 Name = "Home and Furniture",
@@ -82,7 +380,7 @@ namespace Store.Data.Migrations
                 PicturePath = "toys.png"
             };
 
-            context.Categories.AddOrUpdate(c=>c.Name, toys,sport,photo,mobile,home,fash,electric,pc,books);
+            context.Categories.AddOrUpdate(c => c.Name, toys, sport, photo, mobile, home, fash, electric, pc, books);
             context.SaveChanges();
         }
 
@@ -109,7 +407,7 @@ namespace Store.Data.Migrations
             {
                 var store = new RoleStore<IdentityRole>(context);
                 var manager = new RoleManager<IdentityRole>(store);
-                var role = new IdentityRole {Name = "User"};
+                var role = new IdentityRole { Name = "User" };
                 manager.Create(role);
             }
 
@@ -117,7 +415,7 @@ namespace Store.Data.Migrations
             {
                 var store = new RoleStore<IdentityRole>(context);
                 var manager = new RoleManager<IdentityRole>(store);
-                var role = new IdentityRole {Name = "Admin"};
+                var role = new IdentityRole { Name = "Admin" };
                 manager.Create(role);
             }
 
@@ -125,7 +423,7 @@ namespace Store.Data.Migrations
             {
                 var userStore = new UserStore<User>(context);
                 var userManager = new UserManager<User>(userStore);
-                var userToInsert = new User {UserName = "admin@gmail.com", Email = "admin@gmail.com"};
+                var userToInsert = new User { UserName = "admin@gmail.com", Email = "admin@gmail.com" };
 
                 userManager.Create(userToInsert, "123456");
                 userManager.AddToRole(userToInsert.Id, "Admin");
