3c91477865a341260dd2e63cbd258e45c80cca3d
Pavel Ilchev <pavel_ilchev@abv.bg>
2017-03-25 17:13:43 +0200

    seed countries

--- OnlineStore/Store.Data/Migrations/Configuration.cs
+++ OnlineStore/Store.Data/Migrations/Configuration.cs
@@ -1,7 +1,11 @@
 namespace Store.Data.Migrations
 {
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
+    using System.IO;
     using System.Linq;
+    using System.Web;
+    using System.Web.Script.Serialization;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
@@ -17,11 +21,34 @@ namespace Store.Data.Migrations
 
         protected override void Seed(StoreContext context)
         {
+            this.SeedAdmin(context);
+            this.SeedCountries(context);
+        }
+
+        private void SeedCountries(StoreContext context)
+        {
+            string path = HttpContext.Current.Server.MapPath(@"/Content/DataImports/countries.json");
+            using (StreamReader reader = new StreamReader(path))
+            {
+                var json = reader.ReadToEnd();
+                var serializer = new JavaScriptSerializer();
+                var countries = serializer.Deserialize<List<Country>>(json);
+                foreach (var country in countries)
+                {
+                    context.Countries.AddOrUpdate(c => c.Name, country);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedAdmin(StoreContext context)
+        {
             if (!context.Roles.Any(r => r.Name == "User"))
             {
                 var store = new RoleStore<IdentityRole>(context);
                 var manager = new RoleManager<IdentityRole>(store);
-                var role = new IdentityRole { Name = "User" };
+                var role = new IdentityRole {Name = "User"};
                 manager.Create(role);
             }
 
@@ -29,15 +56,15 @@ namespace Store.Data.Migrations
             {
                 var store = new RoleStore<IdentityRole>(context);
                 var manager = new RoleManager<IdentityRole>(store);
-                var role = new IdentityRole { Name = "Admin" };
+                var role = new IdentityRole {Name = "Admin"};
                 manager.Create(role);
             }
-            
+
             if (!context.Users.Any(u => u.UserName == "admin@gmail.com"))
             {
                 var userStore = new UserStore<User>(context);
                 var userManager = new UserManager<User>(userStore);
-                var userToInsert = new User { UserName = "admin@gmail.com", Email = "admin@gmail.com" };
+                var userToInsert = new User {UserName = "admin@gmail.com", Email = "admin@gmail.com"};
 
                 userManager.Create(userToInsert, "123456");
                 userManager.AddToRole(userToInsert.Id, "Admin");
